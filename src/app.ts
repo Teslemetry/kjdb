b67835b750be03d97c987fb6f6749e718f10b75c
Brett <brett.whynot@gmail.com>
2025-02-24 19:47:54 +1000

    Ready

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,9 @@ import Fastify from "fastify";
 import fs from "fs";
 import { JSONSchema } from "json-schema-to-ts";
 import { request as lock } from "piscina-locks";
+//import { request as send } from "undici";
 
-const PATH = "/home/node/cache";
+const PATH = "/home/brett/Teslemetry/kjdb"; //"/home/node/cache";
 const app = Fastify().withTypeProvider<JsonSchemaToTsProvider>();
 
 function deepMerge(target: Record<string, any>, source: Record<string, any>) {
@@ -17,6 +18,27 @@ function deepMerge(target: Record<string, any>, source: Record<string, any>) {
   }
   return target;
 }
+
+/*const peers = config.get<string[]>("peers");
+
+const sender = async (ip: string, id: string, body: string) => {
+  try {
+    const response = await send(`http://${ip}/${id}`, { method: "PUT", body });
+    if (response.statusCode > 299) {
+      throw new Error();
+    }
+    return;
+  } catch (err) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return sender(ip, id, body);
+  }
+};
+
+const replicate = async (id: string, body: string) =>
+  Promise.all(peers.map((ip: string) => sender(ip, id, body)));
+  */
+
+const listeners: Record<string, ((data: any) => void)[]> = {};
 
 app.get(
   "/:id",
@@ -44,6 +66,39 @@ app.get(
   },
 );
 
+app.get(
+  "/:id/next",
+  {
+    schema: {
+      params: {
+        type: "object",
+        properties: {
+          id: { type: "string" },
+        },
+        required: ["id"],
+      } as const satisfies JSONSchema,
+    },
+  },
+  async (request, reply) => {
+    const { id } = request.params;
+    return lock(id, async () => {
+      try {
+        return reply.send(fs.createReadStream(`${PATH}/${id}.json`));
+      } catch (error) {
+        console.error(error);
+        return reply.status(404).send("404");
+      }
+      return new Promise((resolve, reject) => {
+        listeners[id] ??= [];
+        listeners[id].push(resolve);
+      }).then((data) => {
+        delete listeners[id];
+        return reply.send(data);
+      });
+    });
+  },
+);
+
 app.route({
   url: "/:id",
   method: ["POST", "PATCH", "PUT"],
@@ -64,52 +119,40 @@ app.route({
     return lock(id, async () => {
       switch (request.method) {
         case "PUT":
-          return new Promise((resolve, reject) => {
-            const writeStream = fs
-              .createWriteStream(`${PATH}/${id}.json`)
-              .on("error", (err) => {
-                reply.status(404).send();
-                resolve();
-              });
-            request.raw.pipe(writeStream);
-            request.raw.on("end", () => {
-              writeStream.end();
-              reply.status(201).send();
-              resolve();
-            });
-            request.raw.on("error", (error) => {
-              request.log.error(error);
-              writeStream.end();
-              reply.status(500).send();
-              resolve();
-            });
-          });
+          const data = JSON.stringify(request.body);
+          listeners[id]?.forEach((x) => x(data));
+          return fs.promises.writeFile(`${PATH}/${id}.json`, data);
         case "POST":
           return fs.promises.readFile(`${PATH}/${id}.json`, "utf8").then(
-            (raw) =>
-              fs.promises
-                .writeFile(
-                  `${PATH}/${id}.json`,
-                  JSON.stringify({ ...JSON.parse(raw), ...request.body }),
-                )
-                .then(() => reply.status(201).send()),
+            async (raw) => {
+              const data = JSON.stringify({
+                ...JSON.parse(raw),
+                ...request.body,
+              });
+              listeners[id]?.forEach((x) => x(data));
+              return fs.promises
+                .writeFile(`${PATH}/${id}.json`, data)
+                .then(() => reply.status(201).send());
+            },
             (error) => {
               request.log.error(error);
-              reply.status(404).send();
+              return reply.status(404).send();
             },
           );
         case "PATCH":
           return fs.promises.readFile(`${PATH}/${id}.json`, "utf8").then(
-            (raw) =>
-              fs.promises
-                .writeFile(
-                  `${PATH}/${id}.json`,
-                  JSON.stringify(deepMerge(JSON.parse(raw), request.body)),
-                )
-                .then(() => reply.status(201).send()),
+            async (raw) => {
+              const data = JSON.stringify(
+                deepMerge(JSON.parse(raw), request.body),
+              );
+              listeners[id]?.forEach((x) => x(data));
+              return fs.promises
+                .writeFile(`${PATH}/${id}.json`, data)
+                .then(() => reply.status(201).send());
+            },
             (error) => {
               request.log.error(error);
-              reply.status(404).send();
+              return reply.status(404).send();
             },
           );
       }
